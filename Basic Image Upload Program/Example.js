e04b47cbda680e6872e7d0b9d5569b0a1af5bbbf
Chase_Crawford <chasecrawford95@gmail.com>
2023-12-14 00:25:38 -0600

    Added Basic Image Uploader

---
+++ Basic Image Upload Program/Example.js
@@ -0,0 +1,27 @@
+const fileInput = document.getElementById('fileInput');
+const selectedImage = document.getElementById('selectedImage');
+const imageContainer = document.getElementById('imageContainer');
+
+function displaySelectedImage(fileInput, selectedImage, imageContainer) {
+    const selectedFile = fileInput.files[0];
+
+    if (selectedFile) {
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            selectedImage.src = e.target.result;
+            selectedImage.style.display = 'block';
+            imageContainer.style.display = 'block';
+        };
+
+        reader.readAsDataURL(selectedFile);
+    }
+}
+
+fileInput.addEventListener('change', function () {
+    displaySelectedImage(fileInput, selectedImage, imageContainer);
+});
+
+selectedImage.addEventListener('wheel', function (e){
+    
+});
