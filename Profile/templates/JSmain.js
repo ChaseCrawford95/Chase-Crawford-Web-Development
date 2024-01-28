f7eac03b24dfde475330a060ae8d4461595bed15
Chase Crawford <chasecrawford95@gmail.com>
2024-01-28 00:00:58 -0600

    Update

--- Profile/templates/JSmain.js
+++ Profile/templates/JSmain.js
@@ -65,7 +65,7 @@ MobileDevelopment.addEventListener('mouseout', () => {
     RedBall.style.animation = 'RedBallShrink 1s forwards';
     Text3.style.animation = "Text3Rise 1s ease forwards";
 });
-
+// -------------------------------------------------------------------------
 const VerticalLine = document.getElementById('VerticalLine');
 const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
@@ -90,4 +90,101 @@ image3.addEventListener('mouseover', () => {
 })
 image3.addEventListener('mouseout', () => {
     VerticalLine.style = 'none 0.3s ease';
+})
+// -----------------------------------------------------------
+const PythonTopLayer = document.getElementById("PythonTopLayer");
+const PythonColoredBox = document.getElementById("PythonColoredBox");
+const JavaTopLayer = document.getElementById("JavaTopLayer");
+const JavaColoredBox = document.getElementById("JavaColoredBox");
+const CTopLayer = document.getElementById("CTopLayer");
+const CColoredBox = document.getElementById("CColoredBox");
+const AssemblyTopLayer = document.getElementById("AssemblyTopLayer");
+const AssemblyColoredBox = document.getElementById("AssemblyColoredBox");
+const KotlinTopLayer = document.getElementById("KotlinTopLayer");
+const KotlinColoredBox = document.getElementById("KotlinColoredBox");
+const SwiftTopLayer = document.getElementById("SwiftTopLayer");
+const SwiftColoredBox = document.getElementById("SwiftColoredBox");
+const JavaScriptTopLayer = document.getElementById("JavaScriptTopLayer");
+const JavaScriptColoredBox = document.getElementById("JavaScriptColoredBox");
+const SQLiteTopLayer = document.getElementById("SQLiteTopLayer");
+const SQLiteColoredBox = document.getElementById("SQLiteColoredBox");
+const HTMLTopLayer = document.getElementById("HTMLTopLayer");
+const HTMLColoredBox = document.getElementById("HTMLColoredBox");
+const CSSTopLayer = document.getElementById("CSSTopLayer");
+const CSSColoredBox = document.getElementById("CSSColoredBox");
+const XMLTopLayer = document.getElementById("XMLTopLayer");
+const XMLColoredBox = document.getElementById("XMLColoredBox");
+const FXMLTopLayer = document.getElementById("FXMLTopLayer");
+const FXMLColoredBox = document.getElementById("FXMLColoredBox");
+PythonTopLayer.addEventListener("mouseover", () => {
+    PythonColoredBox.style.width = "395px";
+})
+PythonTopLayer.addEventListener("mouseout", () =>{
+    PythonColoredBox.style.width = "0px";
+})
+JavaTopLayer.addEventListener("mouseover",()=>{
+    JavaColoredBox.style.width = "395px";
+})
+JavaTopLayer.addEventListener("mouseout",()=>{
+    JavaColoredBox.style.width = "0px";
+})
+CTopLayer.addEventListener("mouseover",()=>{
+    CColoredBox.style.width = "395px";
+})
+CTopLayer.addEventListener("mouseout", ()=>{
+    CColoredBox.style.width = "0px";
+})
+AssemblyTopLayer.addEventListener("mouseover", ()=>{
+    AssemblyColoredBox.style.width = "395px"
+})
+AssemblyTopLayer.addEventListener("mouseout", ()=>{
+    AssemblyColoredBox.style.width="0px";
+})
+KotlinTopLayer.addEventListener("mouseover", ()=>{
+    KotlinColoredBox.style.width = "395px";
+})
+KotlinTopLayer.addEventListener("mouseout",() =>{
+    KotlinColoredBox.style.width = "0px";
+})
+SwiftTopLayer.addEventListener("mouseover", ()=>{
+    SwiftColoredBox.style.width = "395px"
+})
+SwiftTopLayer.addEventListener("mouseout",()=>{
+    SwiftColoredBox.style.width = "0px"
+})
+JavaScriptTopLayer.addEventListener("mouseover", ()=>{
+    JavaScriptColoredBox.style.width= "395px"
+})
+JavaScriptTopLayer.addEventListener("mouseout", ()=>{
+    JavaScriptColoredBox.style.width= "0px"
+})
+SQLiteTopLayer.addEventListener("mouseover", () =>{
+    SQLiteColoredBox.style.width = "395px";
+})
+SQLiteTopLayer.addEventListener("mouseout", () =>{
+    SQLiteColoredBox.style.width = "0px";
+})
+HTMLTopLayer.addEventListener("mouseover", ()=>{
+    HTMLColoredBox.style.width = "395px";
+})
+HTMLTopLayer.addEventListener("mouseout", ()=>{
+    HTMLColoredBox.style.width = "0px";
+})
+CSSTopLayer.addEventListener("mouseover", ()=>{
+    CSSColoredBox.style.width = "395px"
+})
+CSSTopLayer.addEventListener("mouseout", ()=>{
+    CSSColoredBox.style.width = "0px"
+})
+XMLTopLayer.addEventListener("mouseover", () =>{
+    XMLColoredBox.style.width = "395px"
+})
+XMLTopLayer.addEventListener("mouseout", () =>{
+    XMLColoredBox.style.width = "0px"
+})
+FXMLTopLayer.addEventListener("mouseover", ()=>{
+    FXMLColoredBox.style.width = "395px"
+})
+FXMLTopLayer.addEventListener("mouseout", ()=>{
+    FXMLColoredBox.style.width = "0px"
 })
